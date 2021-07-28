f7cb0a3a6da3de595d992b65e03fd85d45bedba3
hellodeborahuk <29425781+hellodeborahuk@users.noreply.github.com>
2021-07-28 21:41:00 +0100

    Created react app with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        orange: colors.orange,
+        teal: colors.teal,
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
